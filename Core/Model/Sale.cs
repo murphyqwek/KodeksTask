cdc1de866961eb128cede70f77cebda71c2a4f22
murphyqwek <qwekinaft@list.ru>
2026-09-18 19:59:58 +0300

    Add Sale entity to model

---
+++ Core/Model/Sale.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Model
+{
+    public class Sale
+    {
+        public int OrderId { get; init; }
+        public DateOnly OrderDate { get; init; }
+        public int CustomerId { get; init; }
+
+        public required string ProductCategory { get; init; }
+        public required string Region { get; init; }
+
+        public decimal Discount { get; init; }
+
+        public required string PaymentMethod { get; init; }
+
+        public int DeliveryDays { get; init; }
+        public decimal CustomerRating { get; init; }
+        public decimal Revenue { get; init; }
+    }
+}
