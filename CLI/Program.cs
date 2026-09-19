965c1ef07b86945edfd4b21d0e59ea3bb8fb5697
murphyqwek <qwekinaft@list.ru>
2026-09-19 14:42:44 +0300

    Add program cycle to Main in CLI

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -2,14 +2,69 @@
 
 namespace CLI;
 
-public class Program
+public static class Program
 {
-    static void Main(string[] args)
+    public static async Task<int> Main(string[] args)
     {
-        var parser = new CommandLineParser();
+        try
+        {
+            using var cancellationTokenSource = new CancellationTokenSource();
 
-        var commandArgs = parser.Parse(args);
+            Console.CancelKeyPress += (_, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                cancellationTokenSource.Cancel();
+            };
 
+            var parser = new CommandLineParser();
 
+            var arguments = parser.Parse(args);
+
+            if (arguments.IsHelp)
+            {
+                PrintHelp();
+                return 0;
+            }
+
+            var runner = BuildApplicationRunner(arguments);
+
+            await runner.RunAsync(arguments.Options, cancellationTokenSource.Token);
+
+            return 0;
+        }
+        catch (Exception exception)
+        {
+            return GlobalExceptionHandler.Handle(exception);
+        }
+    }
+
+    private static ApplicationRunner.ApplicationRunner BuildApplicationRunner(CommandLineArguments arguments)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void PrintHelp()
+    {
+        Console.WriteLine("""
+            Kodeks Test Task - Sales Analyzer
+
+            Usage:
+              SalesAnalyzer <mode> --input <path> [options]
+
+            Modes:
+                console     Synchronous CSV reading, calculation of three analytics, and output to the console
+                file        Same as console, but the results are saved to a JSON file. The output path is specified via --output
+                async       Asynchronous file reading using StreamReader with ReadLineAsync, and asynchronous JSON writing
+                parallel    Parallel processing of records when calculating analytics
+                di          Use a DI container to register all services
+                full        Combined mode: asynchronous I/O + parallel analytics calculation + dependency injection
+
+            Options:
+              --input <path>        Input CSV file
+              --output <path>       Output file
+              --start-date <date>   Start date
+              --end-date <date>     End date
+              --help                Show help
+            """);
     }
 }
